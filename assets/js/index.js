79c340be44d1a4b2c4843001d58ec4c6e66a3bf2
Patrick Hladun <patrickhladun@gmail.com>
2023-12-15 21:19:57 +0000

    feat: create a new task on plus button click and log the object

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,13 +9,31 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 class Task {
+  /**
+   * Task constructor function to create a new task object with the following properties: name, dueDate, timeSpent, id
+   * @param {*} name
+   * @param {*} dueDate
+   * @param {*} creationDate
+   */
   constructor(name) {
+    this.id = "dfh9237623gf";
     this.name = name;
+    this.dueDate = "";
+    this.creationDate = new Date();
   }
+
   addTask(e) {
+    // Prevent default form submission so the page doesn't reload
     e.preventDefault();
-    this.name = document.getElementById("task-name").value.trim();
-    console.log(this.name);
+
+    // Get the task name from the form
+    const taskName = document.getElementById("task-name").value.trim();
+
+    // Create a new task object
+    const newTask = new Task(taskName);
+
+    console.log(newTask);
+
     document.getElementById("task-form").reset();
   }
 }
